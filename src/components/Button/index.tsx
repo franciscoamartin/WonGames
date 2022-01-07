6d54e2fe860b6be0e0d8ff31f7c7d125da3cf658
Francisco Martin <francisco.martin@ambevtech.com.br>
2022-01-07 20:52:51 -0300

    update snapshot tests

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,29 @@
+import { ButtonHTMLAttributes } from 'hoist-non-react-statics/node_modules/@types/react'
+import { AnchorHTMLAttributes } from 'react'
+import * as S from './styles'
+
+type ButtonTypes =
+  | AnchorHTMLAttributes<HTMLAnchorElement>
+  | ButtonHTMLAttributes<HTMLButtonElement>
+
+export type ButtonProps = {
+  size?: 'small' | 'medium' | 'large'
+  fullWidth?: boolean
+  icon?: JSX.Element
+  as?: React.ElementType
+} & ButtonTypes
+
+const Button = ({
+  children,
+  size = 'medium',
+  fullWidth = false,
+  icon,
+  ...props
+}: ButtonProps) => (
+  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} {...props}>
+    {!!icon && icon}
+    {!!children && <span>{children}</span>}
+  </S.Wrapper>
+)
+
+export default Button
