28cb3f543b78974855869070a5a4372d870807c2
Francisco Martin <francisco.martin@ambevtech.com.br>
2022-01-20 19:52:25 -0300

    add highlight component

--- src/components/Highlight/styles.ts
+++ src/components/Highlight/styles.ts
@@ -2,10 +2,31 @@ import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 import { HighlightProps } from '.'
 
-type WrapperProps = Pick<HighlightProps, 'backgroundImage'>
+type WrapperProps = Pick<HighlightProps, 'backgroundImage' | 'alignment'>
+
+const wrapperModifiers = {
+  right: () => css`
+    grid-template-areas: 'floatimage content';
+    grid-template-columns: 1.3fr 2fr;
+    ${Content} {
+      text-align: right;
+    }
+  `,
+  left: () => css`
+    grid-template-areas: 'content floatimage';
+    grid-template-columns: 2fr 1.3fr;
+    ${Content} {
+      text-align: left;
+    }
+
+    ${FloatImage} {
+      justify-self: end;
+    }
+  `
+}
 
 export const Wrapper = styled.section<WrapperProps>`
-  ${({ backgroundImage }) => css`
+  ${({ backgroundImage, alignment }) => css`
     position: relative;
     background-image: url(${backgroundImage});
     background-position: center center;
@@ -24,13 +45,29 @@ export const Wrapper = styled.section<WrapperProps>`
     ${media.greaterThan('medium')`
       height: 32rem;
     `}
+
+    ${wrapperModifiers[alignment!]()}
+  `}
+`
+
+export const FloatImage = styled.img`
+  ${({ theme }) => css`
+    z-index: ${theme.layers.base};
+    align-self: end;
+    grid-area: floatimage;
+    max-height: 23rem;
+    max-width: 100%;
+
+    ${media.greaterThan('medium')`
+      max-height: 32rem;
+    `}
   `}
 `
 
 export const Content = styled.div`
   ${({ theme }) => css`
     z-index: ${theme.layers.base};
-    text-align: right;
+    grid-area: content;
     padding: ${theme.spacings.xsmall};
 
     ${media.greaterThan('medium')`
--- src/components/Highlight/test.tsx
+++ src/components/Highlight/test.tsx
@@ -1,6 +1,6 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
-
+import * as S from './styles'
 import Highlight from '.'
 
 const props = {
@@ -29,6 +29,43 @@ describe('<Highlight />', () => {
 
     expect(container.firstChild).toHaveStyle({
       backgroundImage: `url(${props.backgroundImage})`
+    })
+  })
+
+  it('should render float image', () => {
+    renderWithTheme(<Highlight {...props} floatImage="/float-image.png" />)
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      '/float-image.png'
+    )
+  })
+
+  it('should render align right by default', () => {
+    const { container } = renderWithTheme(<Highlight {...props} />)
+
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'floatimage content'"
+    )
+
+    expect(container.firstChild).toHaveStyleRule('text-align', 'right', {
+      modifier: `${S.Content} `
+    })
+  })
+
+  it('should render align left', () => {
+    const { container } = renderWithTheme(
+      <Highlight {...props} alignment="left" />
+    )
+
+    expect(container.firstChild).toHaveStyleRule(
+      'grid-template-areas',
+      "'content floatimage'"
+    )
+
+    expect(container.firstChild).toHaveStyleRule('text-align', 'left', {
+      modifier: `${S.Content} `
     })
   })
 })
